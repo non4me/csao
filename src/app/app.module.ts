1b625bca46a0a2f713c5201c9ca01f9f706d6dcd
Vladimir Troyanenko <post@itman.ru>
2017-12-30 00:28:43 +0100

    add ngsw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {ServiceWorkerModule} from '@angular/service-worker';
 
+import { environment } from '../environments/environment';
 
 import {AppComponent} from './app.component';
 import {CryptoService} from './_shared/services/crypto/crypto.service';
@@ -10,7 +12,10 @@ import {CryptoService} from './_shared/services/crypto/crypto.service';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: environment.production
+    })
   ],
   providers: [CryptoService],
   bootstrap: [AppComponent]
